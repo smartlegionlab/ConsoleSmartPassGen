d8ea51bd04e7a5e39e0596ee7e69c717abd73a79
A.A Suvorov <smartlegiondev@gmail.com>
2024-05-16 17:40:33 +0700

    ConsoleSmartPassGen v0.1.0

---
+++ ConsoleSmartPassGen/Program.cs
@@ -0,0 +1,109 @@
+﻿// Console Smart Password Generator
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+class Strings
+{
+    public static readonly string UppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static readonly string LowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    public static readonly string Digits = "0123456789";
+    public static readonly string SpecialCharacters = "!@#$%^&*()";
+}
+
+class CharacterPrinter
+{
+    public void PrintCharacter(char symbol, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(symbol);
+        }
+        Console.WriteLine();
+    }
+}
+
+
+class SmartRandomGen
+{
+    private int GetSeed(string input)
+    {
+        byte[] hash;
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+        }
+
+        int seed = BitConverter.ToInt32(hash, 0);
+        return seed;
+    }
+
+    public string GenerateSmartRandomString(int length, string secret)
+    {
+        int seedHash = this.GetSeed(secret);
+        Random rand = new Random(seedHash);
+        string allCharacters = Strings.UppercaseLetters + Strings.LowercaseLetters + Strings.Digits + Strings.SpecialCharacters;
+        char[] password = new char[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            int randomIndex = rand.Next(0, allCharacters.Length);
+            password[i] = allCharacters[randomIndex];
+        }
+
+        return new string(password);
+    }
+
+}
+
+
+class ConsoleSmartPasswordGeneratorApp
+{
+    static void Main(string[] args)
+    {
+        CharacterPrinter printer = new CharacterPrinter();
+        Console.WriteLine("*** Console Smart Password Generator ***");
+        SmartRandomGen smartRandomGen = new SmartRandomGen();
+        string password = "";
+        Console.WriteLine("Введите длину пароля: ");
+        printer.PrintCharacter('-', 23);
+        int passwordLength = 0;
+        try
+        {
+            passwordLength = Convert.ToInt32(Console.ReadLine());
+            printer.PrintCharacter('-', 23);
+        }
+        catch (Exception e)
+        {
+            passwordLength = 12;
+            Console.WriteLine("Ошибка! Вы не ввели длину пароля. Использую 12 символов.");
+            printer.PrintCharacter('-', 23);
+        }
+        if (passwordLength <= 0)
+        {
+            passwordLength = 12;
+            Console.WriteLine("Ошибка! Неверная длина пароля. Использую 12 символов.");
+            printer.PrintCharacter('-', 23);
+        }
+        else if (passwordLength > 1000)
+        {
+            passwordLength = 1000;
+            Console.WriteLine("Ошибка! Пароль не может быть длинее 1000 символов. Использую 1000 символов.");
+            printer.PrintCharacter('-', 23);
+        }
+        string secretPhrase = "";
+        Console.WriteLine("Введите секретную фразу: ");
+        printer.PrintCharacter('-', 23);
+        secretPhrase = Console.ReadLine();
+        printer.PrintCharacter('-', 23);
+        password = smartRandomGen.GenerateSmartRandomString(passwordLength, secretPhrase);
+        Console.WriteLine(password);
+        printer.PrintCharacter('-', 23);
+        // Ожидание реакции пользователя
+        Console.WriteLine("Нажмите <Enter> для выхода...");
+        Console.Read();
+        printer.PrintCharacter('-', 23);
+        Console.WriteLine("=== Smart Legion Lab ===");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # ConsoleSmartPassGen
-Console Smart Password generator for windows.
+
+***
+
+> Console generator of smart passwords linked to a secret phrase.
+
+***
+
+> WARNING! Not compatible with smart password generators written in Python. Smart passwords will be different.
+
+***
+
+Author and developer: ___A.A Suvorov___
+
+***
+
+## Images:
+
+![logo](https://github.com/smartlegionlab/ConsolePassGen/raw/master/data/images/ConsoleSmartPassGen.png)
+
+***
+
+> Download: [ConsolePassGen](https://github.com/smartlegionlab/ConsoleSmartPassGen/releases/download/v0.1.0/ConsoleSmartPassGen.zip);
+
+***
+
+## Requirements:
+
+[.NET 8.0](https://aka.ms/dotnet-core-applaunch?missing_runtime=true&arch=x64&rid=win-x64&os=win10&apphost_version=8.0.1)
+
+***
+
+## Disclaimer of liability:
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+***
+
+## Copyright:
+    --------------------------------------------------------
+    Licensed under the terms of the BSD 3-Clause License
+    (see LICENSE for details).
+    Copyright © 2018-2024, A.A Suvorov
+    All rights reserved.
+    --------------------------------------------------------
